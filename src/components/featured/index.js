1b658059f9391e56f2498a6422684303eea4ecdb
vcaster <ainaniran@yahoo.com>
2019-09-06 15:05:32 -0400

    Carousel added

---
+++ src/components/featured/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Carrousel from './Carrousel';
+
+const Featured = () => {
+    return (
+        <div style={{position:'relative'}}>
+
+            <Carrousel/>
+
+            <div className="artist_name">
+                <div className="wrapper">
+                    Burna<br></br> Boy
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Featured;
